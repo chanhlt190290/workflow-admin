69493c99978dafc0c415a1e146af45d8e1c927db
chanhlt <chanhlt190290@gmail.com>
2018-06-06 19:13:43 +0900

    add source

---
+++ src/main/java/com/chanhlt/workflow/admin/WorkflowAdminApplication.java
@@ -0,0 +1,12 @@
+package com.chanhlt.workflow.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowAdminApplication.class, args);
+	}
+}
